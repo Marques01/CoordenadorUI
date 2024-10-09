af2ee4c49472f303c0762730fd3e28370df7cae1
Marques01 <cleiton.marques97@hotmail.com>
2024-10-09 18:15:10 -0300

    criação da viewmodel para login

--- src/UI/Pages/Account/SignIn.razor.cs
+++ src/UI/Pages/Account/SignIn.razor.cs
@@ -1,10 +1,47 @@
-﻿namespace UI.Pages.Account
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using UI.ViewModels;
+
+namespace UI.Pages.Account
 {
     public partial class SignIn
     {
+        private bool
+            _isLoading = false;
+
+        private readonly AccountViewModel _accountViewModel = new AccountViewModel();
+
+        private EditContext? _editContext;
+
+        private ElementReference _elementReference;
+
         protected override void OnInitialized()
         {
-            base.OnInitialized();
+            _editContext = new EditContext(_accountViewModel);
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await _elementReference.FocusAsync();
+            }
+        }
+
+        private async Task SubmitAsync()
+        {
+            if (_editContext is not null && _editContext.Validate())
+            {
+                _isLoading = true;
+
+                // Do something with the validated data
+                await Task.Delay(4000);
+
+                // Redirect to another page
+
+                _isLoading = false;
+                return;
+            }
         }
     }
 }
---
+++ src/UI/ViewModels/AccountViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace UI.ViewModels
+{
+    public class AccountViewModel
+    {
+        private string
+            _email = string.Empty,
+            _password = string.Empty;
+
+
+        [Required(AllowEmptyStrings = false)]
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                string cleanedValue = Regex.Replace(
+                    value.Trim().ToLower(), @"[^0-9-@.a-zA-Z]", string.Empty);
+                _email = cleanedValue;
+            }
+        }
+
+        [Required(AllowEmptyStrings = false)]
+        public string Password
+        {
+            get => _password;
+            set => _password = value.Trim();
+        }
+
+        public bool RememberMe { get; set; }
+    }
+}
